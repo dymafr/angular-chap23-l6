88553c1de7c96d73452a42ffc628205ce5685ae1
dymafr <erwan1309@hotmail.com>
2021-01-12 11:29:58 +0100

    3 files modified

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -3,9 +3,11 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatMenuModule } from "@angular/material/menu";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
 const MATERIALS = [
   MatButtonModule,
+  MatCheckboxModule,
   MatIconModule,
   MatMenuModule,
   MatSidenavModule
