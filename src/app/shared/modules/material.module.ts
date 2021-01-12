4f2b69d3e06ee62157f1d49f4f89f98b8d061432
dymafr <erwan1309@hotmail.com>
2021-01-12 11:04:28 +0100

    1 file modified

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatMenuModule } from "@angular/material/menu";
 
-const MATERIALS = [MatButtonModule, MatIconModule, MatSidenavModule];
+const MATERIALS = [
+  MatButtonModule,
+  MatIconModule,
+  MatMenuModule,
+  MatSidenavModule
+];
 
 @NgModule({
   imports: MATERIALS,
